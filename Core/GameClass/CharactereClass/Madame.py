978b8cab403bc3dd2401e74393d2826b519159da
Needoliprane <louis-auguste.dumas@epitech.eu>
2020-12-21 02:50:25 +0100

    [FIX] madame actions

--- Core/GameClass/CharactereClass/Madame.py
+++ Core/GameClass/CharactereClass/Madame.py
@@ -4,6 +4,6 @@ class Madame(Player):
 
     def actions(self, room, otherPerson, otherPersons):
         if self.room.isLock() == True:
-            self.room.lockTheRoom()
-        if self.room.isLock() == False:
             self.room.unlockTheRoom()
+        else :
+            self.room.lockTheRoom()
