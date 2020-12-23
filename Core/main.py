4561a661e4948a69c24a292179d60f9ff2a7460e
Needoliprane <louis-auguste.dumas@epitech.eu>
2020-12-23 09:15:17 +0100

    [update] gameloop almost ready

--- Core/main.py
+++ Core/main.py
@@ -1,9 +1,13 @@
 #!/usr/local/bin/python
 
 from GameClass.Game import Game
+from GameClass.CharactereClass.Meg import Meg
+from GameClass.Room import Room
 
 def master():
     gameController = Game(8, 8)
+    gameController.initGame()
+    gameController.GameLoop()
 
 if __name__ == "__main__":
     master()
