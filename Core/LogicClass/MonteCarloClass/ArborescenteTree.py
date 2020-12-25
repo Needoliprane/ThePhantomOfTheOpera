a5114ad0085fd539017e7b45a07ca86d73a9980c
Needoliprane <louis-auguste.dumas@epitech.eu>
2020-12-25 18:45:24 +0100

    [Update] algo

--- Core/LogicClass/MonteCarloClass/ArborescenteTree.py
+++ Core/LogicClass/MonteCarloClass/ArborescenteTree.py
@@ -5,22 +5,31 @@ class ArborescenteTree:
         self.headCell = Cell(True, -1, None)
 
 #---------------------------------------------- Logical part of arborescenteTree
-    def checkPresenceInTheNode(self, playerId, node):
+    def checkPresenceInTheNode(self, playerId, node, value=1):
         for cell in node:
             if playerId == cell.playerId:
-                cell.value += 1
+                cell.value += value
                 cell.visit += 1
                 cell.refreshUCB()
                 return (True)
         return False
 
-    def addCoupable(self, playerId):
-        if (self.checkPresenceInTheNode(playerId, self.headCell.childCell) == True):
+    def checkPresenceInTheNodeMove(self, playerId, node, value=1):
+        for cell in node:
+            if playerId == cell.playerId:
+                cell.value += value
+                cell.visit += 1
+                cell.refreshUCB()
+                return (True, cell.value)
+        return (False, 0)
+
+    def addPossibilities(self, id, value=1):
+        if (self.checkPresenceInTheNode(id, self.headCell.childCell) == True):
             return
-        self.headCell.childCell.append(Cell(False, playerId, self.headCell))
+        self.headCell.childCell.append(Cell(False, id, self.headCell))
         for cell in self.headCell.childCell:
-            if (cell.playerId == playerId) :
-                cell.value += 1
+            if (cell.playerId == id) :
+                cell.value += value
                 cell.visit += 1
             cell.refreshUCB()
 
@@ -34,6 +43,18 @@ class ArborescenteTree:
             res.append(cell.refreshUCB())
         if (res.count(max(res)) > 1):
             return
+        if (node.childCell[res.index(max(res))].childCell == []):
+            return (node.childCell[res.index(max(res))].playerId)
+        return self.chooseLeaf(node.childCell[res.index(max(res))])
+
+    def chooseLeafMove(self, node):
+        if (node == None or node.childCell == []):
+            return
+        res = []
+        for cell in node.childCell:
+            cell.visit += 1
+            cell.parentCell.childVisit += 1
+            res.append(cell.refreshUCB())
         if (node.childCell[res.index(max(res))].childCell == []):
             return (node.childCell[res.index(max(res))].playerId)
         return self.chooseLeaf(node.childCell[res.index(max(res))])
